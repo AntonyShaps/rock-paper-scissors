df10cbf0db09d029fd0fd50b67d953a2064da8c6
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2023-10-26 20:38:29 +0200

    Add function playRound(), variables are inside the function

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const listOfChoices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return listOfChoices[Math.floor(Math.random()*listOfChoices.length)]
+    return listOfChoices[Math.floor(Math.random()*listOfChoices.length)];
+}
+
+
+
+function playRound(){
+    var playerSelection = prompt("Make your choice!").toLowerCase();
+    var computerSelection = getComputerChoice();
+    var draw = `buddy, it's a draw! ${playerSelection} can't beat ${computerSelection}`;
+    var win = `buddy, you have won! ${playerSelection} beats ${computerSelection}`;
+    var lose = `buddy, you have lost! ${playerSelection} is beaten by ${computerSelection}`;
+    if (playerSelection === computerSelection) {
+        return draw;
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return win;
+    } else if (playerSelection === "paper" && computerSelection ==="rock" ) {
+        return win;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return win;
+    } else {
+        return lose
+    }
+
 }
